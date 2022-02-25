a63d8c134a636da89e33dbd081c78ffb68b104ea
iathestupid <98940452+iathestupid@users.noreply.github.com>
2022-02-25 22:22:15 +0700

    Full Comnmit No More Main Menu Left Now Right Pls

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -94,7 +94,7 @@ class MainMenuState extends MusicBeatState
 			menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
 			menuItem.animation.play('idle');
 			menuItem.ID = i;
-			menuItem.x = 100;
+			menuItem.x = 10;
 			menuItems.add(menuItem);
 			var scr:Float = (optionShit.length - 4) * 0.135;
 			if(optionShit.length < 6) scr = 0;
